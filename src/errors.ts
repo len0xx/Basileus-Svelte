6eb9256711836e80ac2d171217f8a0f8462b36d5
Prokhor Minin <prokhor@len0xx.space>
2022-02-20 22:52:11 +0500

    New errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,8 @@
 export const UNKNOWN_ERROR = 1
 export const EMPTY_FIELDS = 2
 export const UNAUTHORIZED = 3
+export const NO_PERMISSION = 4
+export const NOT_FOUND = 5
 
 export const description = (err: number): string => {
 	if (err === EMPTY_FIELDS) {
