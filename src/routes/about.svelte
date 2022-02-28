5db2f2c4d8a659a819834b6ed1229154b1833e46
Prokhor Minin <prokhor@len0xx.space>
2022-02-28 22:43:46 +0500

    Improve About page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,28 @@
+<script lang="ts">
+	import expressLogo from '../node_modules/images/express.svg'
+	import typescriptLogo from '../node_modules/images/typescript.svg'
+	import svelteLogo from '../node_modules/images/svelte.svg'
+	import mongodbLogo from '../node_modules/images/mongodb.svg'
+</script>
+
 <style lang="sass">
 	a
 		color: blue
 		&:hover
 			text-decoration: underline
+
+	.tech-stack
+		display: grid
+		grid-template-columns: repeat(4, 1fr)
+		gap: 1em
+		margin: 2em 0
+		max-width: 100%
+		span
+			display: grid
+			place-items: center
+			max-width: 100%
+			img
+				height: 32px
 </style>
 
 <svelte:head>
@@ -11,6 +31,29 @@
 
 <section class="container">
 	<h1>About</h1>
-	
 	<p>Read more about this Svelte project on the <a target="_BLANK" href="https://github.com/len0xx/Basileus-Svelte">GitHub page</a></p>
+	<br>
+	<h2>Technologies used in this application:</h2>
+	<div class="tech-stack">
+		<span>
+			<a href="https://svelte.dev" target="_BLANK">
+				<img src={svelteLogo} alt="Svelte Logo" title="Svelte">
+			</a>
+		</span>
+		<span>
+			<a href="https://expressjs.com" target="_BLANK">
+				<img src={expressLogo} alt="ExpressJS Logo" title="ExpressJS">
+			</a>
+		</span>
+		<span>
+			<a href="https://mongodb.com" target="_BLANK">
+				<img src={mongodbLogo} alt="MongoDB Logo" title="MongoDB">
+			</a>
+		</span>
+		<span>
+			<a href="https://typescriptlang.org" target="_BLANK">
+				<img src={typescriptLogo} alt="TypeScript Logo" title="TypeScript">
+			</a>
+		</span>
+	</div>
 </section>
