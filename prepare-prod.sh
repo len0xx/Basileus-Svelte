4cf66a0a443ecaa8cdfd6f3ae64fc0141492e464
Prokhor Minin <prokhor@len0xx.space>
2022-02-28 16:36:38 +0500

    Prepare production build script

---
+++ prepare-prod.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Скрипт для сборки продакшн версии сайта
+
+# Название директории, в которую будет помещена сборка
+dir="build-prod"
+
+rm -rf $dir
+mkdir $dir
+cp -r __sapper__/build/* $dir
+cp package*.json $dir
+sed -i -e 's/__sapper__\/build/./g' $dir/server/server.js
+rm $dir/server/server.js-e
+echo "Production version is ready in $dir directory"
