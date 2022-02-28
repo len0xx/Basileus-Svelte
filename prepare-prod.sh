d5923ae37af81d16cdac5e3ce01a3770f95e2957
Prokhor Minin <prokhor@len0xx.space>
2022-02-28 16:45:03 +0500

    Add static folder to production build

--- prepare-prod.sh
+++ prepare-prod.sh
@@ -8,6 +8,7 @@ dir="build-prod"
 rm -rf $dir
 mkdir $dir
 cp -r __sapper__/build/* $dir
+cp -r static/* $dir
 cp package*.json $dir
 sed -i -e 's/__sapper__\/build/./g' $dir/server/server.js
 rm $dir/server/server.js-e
